3aae6b9dc8c61a2007ddd96b4e791eb1147c9519
j e <j e>
2016-09-25 14:03:28 +0200

    Added [StringLength()] attribute to the Description property of the Article Model.

--- Nuntius/Models/ViewModels.cs
+++ Nuntius/Models/ViewModels.cs
@@ -13,6 +13,5 @@ namespace Nuntius.Models
         public IList<Comment> Comments { get; set; }
         public Subscription Subscription { get; set; }
         public int Favourite { get; set; }
-    
-}
+    }
 }
