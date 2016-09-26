2e07527b9db8820947381a6ed4b01d6a908139a7
j e <j e>
2016-09-26 16:37:42 +0200

    Modified Top News, Latest News and Contact.

--- Nuntius/Controllers/HomeController.cs
+++ Nuntius/Controllers/HomeController.cs
@@ -52,7 +52,25 @@ namespace Nuntius.Controllers
 
         public ActionResult Top()
         {
-            return View();
+            var list = new List<string>(new[] { "bbc-news", /*"associated-press","bbc-sport",  "bloomberg",
+				"business-insider", "business-insider-uk", "buzzfeed", "cnbc", "cnn", "daily-mail", "engadget", "entertainment-weekly",
+				"espn", "financial-times", "focus", "google-news", "ign", "independent", "mashable", "metro", "mirror", "new-scientist",
+				"newsweek","reuters", "sky-news", "techcrunch", "techradar", "the-guardian-uk", "the-huffington-post", "the-new-york-times",
+				"the-telegraph", "the-verge", "the-wall-street-journal", "the-washington-post", "time"*/});
+
+
+
+            var rng = new Random();
+            var randomElement = list[rng.Next(list.Count)];
+            string source = randomElement;
+            WebClient c = new WebClient();
+            string downloadjson = "https://newsapi.org/v1/articles?source=" + source + /*"&sortBy="+sortedby+*/
+                                  "&apiKey=346e17ce990f4aacac337fe81afb6f50";
+            var json =
+                c.DownloadString(downloadjson);
+            Newsheadline newsheadline = Newtonsoft.Json.JsonConvert.DeserializeObject<Newsheadline>(json);
+            @ViewBag.Source = newsheadline.Source;
+            return View(newsheadline);
         }
 
     }
